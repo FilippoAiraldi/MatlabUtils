71d23793fd18e875ab59111fbcd79f7b4ea19ce4
Filippo Airaldi <filippoairaldi@gmail.com>
2021-06-10 22:16:14 +0200

    added utils function for matrix creation

---
+++ utils/ones_like.m
@@ -0,0 +1,8 @@
+function a = ones_like(b)
+    % ONES_LIKE Array of ones with same size.
+    %
+    %   a = ones_like(b) Creates a matrix/array of ones
+	%		with the same shape as b
+	
+	a = ones(size(b));
+end
---
+++ utils/zeros_like.m
@@ -0,0 +1,8 @@
+function a = zeros_like(b)
+    % ZEROS_LIKE Array of zeros with same size.
+    %
+    %   a = zeros_like(b) Creates a matrix/array of zeros
+	%		with the same shape as b
+	
+	a = zeros(size(b));
+end
