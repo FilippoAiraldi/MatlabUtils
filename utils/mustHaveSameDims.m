077debc9289c3b68ad41f75f9c4bf1ae76e0ae19
Filippo Airaldi <filippoairaldi@gmail.com>
2021-07-06 09:10:07 +0200

    refactored some validators and others

---
+++ utils/mustHaveSameDims.m
@@ -0,0 +1,21 @@
+function mustHaveSameDims(A, B, n, m)
+    % MUSTHAVESAMEDIMS Validates that A and B have the same size
+    %   along the specified dimensions n and m, respectively.
+    %
+    %   mustHaveSameDims(A, B, n, m) Compares the size along the 
+    %       n-th dimension of A with that along the m-th dimension of B, 
+    %       and if they do not equal, throws an error.
+    %   
+    %   mustHaveSameDims(A, B, n) Compares sizes of A and B along
+    %       the same dimension n.
+    
+    if nargin < 4
+       m = n; 
+    end
+    
+    sA = size(A, n);
+    sB = size(B, m);
+    assert(sA == sB, ...
+        sprintf("Size of A along the %i-th dim is different from the size of B along the %i-th dim (%i vs %i).", ...
+        n, m, sA, sB))
+end
--- utils/zeros_like.m
+++ utils/zeros_like.m
@@ -2,7 +2,7 @@ function a = zeros_like(b)
     % ZEROS_LIKE Array of zeros with same size.
     %
     %   a = zeros_like(b) Creates a matrix/array of zeros
-	%		with the same shape as b.
+	%		with the same shape and type as b.
 	
-	a = zeros(size(b));
+	a = zeros(size(b), 'like', b);
 end
